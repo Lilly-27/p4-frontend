1c4a5e45bcc43aab49601a0a3e138b18b10bc1a9
Lilly-27 <ghcecilia1@gmail.com>
2022-07-06 17:10:13 -0500

    scaffolding

--- hiatus/src/App.js
+++ hiatus/src/App.js
@@ -3,7 +3,6 @@ import LandingPage from './Pages/LandingPage/LandingPage';
 import LogIn from './Pages/LogIn/LogIn';
 import SignUp from './Pages/SignUp/SignUp';
 import Clinics from './Pages/Clinics/Clinics';
-// import Reviews from './Pages/Reviews/Reviews';
 import StoriesViewPage from './Pages/StoriesViewPage/StoriesViewPage';
 import Account from './Pages/MyAccount/Account';
 import './App.css';
@@ -20,7 +19,6 @@ function App() {
         <Route exact path='/myaccount' element ={<Account/>}></Route>
 
         <Route exact path='/clinics' element ={<Clinics/>}></Route>
-        {/* <Route exact path='/reviews' element ={<Reviews/>}></Route> */}
         <Route exact path='/stories' element ={<StoriesViewPage/>}></Route>
        
 
