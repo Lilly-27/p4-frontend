4f728a36a43a6dde907397588c6c878472ec8ce0
Lilly-27 <ghcecilia1@gmail.com>
2022-07-05 21:15:56 -0500

    scaffolding

---
+++ hiatus/src/Pages/Clinics/Clinics.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Clinics = () => {
+  return (
+    <div>This is our Clinics page</div>
+  )
+}
+
+export default Clinics
---
+++ hiatus/src/Pages/StoriesViewPage/StoriesViewPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StoriesViewPage() {
+  return (
+    <div>StoriesViewPage</div>
+  )
+}
+
+export default StoriesViewPage
