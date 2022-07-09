4d838eae63cce9fdf366a7124af932abf92ce7c9
Lilly-27 <ghcecilia1@gmail.com>
2022-07-09 17:38:59 -0500

    Map Wrapper Done.

--- hiatus/src/Pages/LandingPage/LandingPage.jsx
+++ hiatus/src/Pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import NavBar from '../NavBar/NavBar'
 import About from '../About/About'
-import Map from '../Map/Map'
 import Stories from '../Stories/Stories'
 import Resources from '../Resources/Resources'
 import Donate from '../Donate/Donate'
 import Footer from '../Footer/Footer'
+import MapWrapper from '../../Components/Map/MapWrapper'
 
 
 const LandingPage = () => {
@@ -13,7 +13,6 @@ const LandingPage = () => {
     <div>
       <NavBar />
       <About />
-      <Map />
       <Stories />
       <Resources />
       <Donate />
