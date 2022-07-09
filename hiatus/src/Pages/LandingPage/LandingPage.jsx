e4aead58f6e674b4967091ff658f7c2bc2a1f2ed
tellydelly <martinwilson450@gmail.com>
2022-07-09 10:51:52 -0700

    Created state view page with conditional rendering of api response

--- hiatus/src/Pages/LandingPage/LandingPage.jsx
+++ hiatus/src/Pages/LandingPage/LandingPage.jsx
@@ -6,11 +6,13 @@ import Stories from '../Stories/Stories'
 import Resources from '../Resources/Resources'
 import Donate from '../Donate/Donate'
 import Footer from '../Footer/Footer'
+import StatePage from '../StatePage/StatePage'
 
 const LandingPage = () => {
   return (
     <div>
       <NavBar />
+      <StatePage statename={"62c781796d098a452182184f"}/>
       <About />
       <Map />
       <Stories />
