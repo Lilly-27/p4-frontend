542f672dcf1eca02afc8c30dfe44cb34003a28a5
Lilly-27 <ghcecilia1@gmail.com>
2022-07-15 00:04:08 -0500

    edited carousel, about and added css for photos

--- hiatus/src/Pages/About/About.jsx
+++ hiatus/src/Pages/About/About.jsx
@@ -6,51 +6,95 @@ import Carousel from 'react-bootstrap/Carousel';
 function About() {
   return (
     <>
-    <div id="About">
-      <div id="Carousel">
+    <div id="Carousel">
+      <div id="Carousel-container">
       <Carousel fade>
+
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://i.imgur.com/F7sYX6g.jpg"
+          src="https://i.imgur.com/kiVzpEB.png"
           alt="First slide"
         />
         <Carousel.Caption>
-          <p>Photo By Clay Banks from Unsplash</p>
+          <h3><span>Preparing for a life<br />
+          post-Roe where everyone has<br/>
+          access to information</span></h3>
+          <a href="#About"><span id="text">About</span></a>
+
         </Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://i.imgur.com/zgh7xpM.jpg"
+          src="https://i.imgur.com/wmRXyfT.png"
           alt="Second slide"
         />
-
         <Carousel.Caption>
-          <p>Photo By Michelle Ding from Unsplash (@michelleding)</p>
+        <h3><span>Information is Power <br />
+          Stay informed during <br/>
+          this Hiatus</span></h3>
+          <a href="#Map"><span id="text">Map</span></a>
         </Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://i.imgur.com/hq6jIBj.jpg"
+          src="https://i.imgur.com/uX8s3yb.png"
           alt="Third slide"
         />
-
         <Carousel.Caption>
-          <p> Photo By Gayatri Malhotra from Unsplash</p>
+        <h3><span>Our Stories Matter <br />
+          Feel Empowered to <br/>
+          Share Your Story</span></h3>
+          <a href="#Stories"><span id="text">Stories</span></a>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i.imgur.com/S9Sj59C.png"
+          alt="Fourth slide"
+        />
+        <Carousel.Caption>
+        <h3><span>Support means Resources <br />
+          Check out resources <br/>
+          available to you</span></h3>
+          <a href="#Resources"><span id="text">Resource Hub</span></a>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i.imgur.com/LKYckK7.png"
+          alt="Fifth slide"
+        />
+        <Carousel.Caption>
+        <h3><span>Empowered People <br/>Empower People <br />
+          Donate to Abortion Funds</span></h3>
+          <a href="#Donate"><span id="text">Donate</span></a>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
       </div>
       
 
-      <div className="about-text">
-        
-        
-        <p></p>
-      This is our about section.
-        This is our about section. We are going to write a short summary of Hiatus.
+      <div className="About">
+        <div>
+          <img src="https://i.imgur.com/fZmqEvT.png" alt="women protesting for rights" />
+        </div>
+
+        <div id="about-two">
+        <p id="about-intro">After SCOTUS overturned Roe v. Wade on June 24, 2022, we knew people with power would do their best to obscure information on abortion services.
+          {/* Many of our grandmothers shared their stories pre-Roe, and it was a life full of unnecessary and cruel pain. */}
+          In hopes of being of service during this unexpected and dangerous interuption in women's rights, 
+          we -- Martin & I -- decided to create a resource for people seeking abortion care. Below, there is an interactive map that shows you which states are protecting access to abortion services, which states are restricting access, and which states are banning them, according to The New York Times' IG posts.</p>
+        </div>
+      
       </div>
     </div>
     </>
