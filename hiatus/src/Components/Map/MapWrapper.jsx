053a9c40f8f642703494021dcae19dfa562192e4
tellydelly <martinwilson450@gmail.com>
2022-07-15 16:46:52 -0700

    created reformmatted waiting period and gestational limits components

--- hiatus/src/Components/Map/MapWrapper.jsx
+++ hiatus/src/Components/Map/MapWrapper.jsx
@@ -1,29 +1,37 @@
 import React, { useState } from 'react'
-import {
-    GoogleMap,
-    LoadScript,
-    Marker,
-    InfoWindow,
-} from '@react-google-maps/api'
+// import {
+//     GoogleMap,
+//     LoadScript,
+//     Marker,
+//     InfoWindow,
+// } from '@react-google-maps/api'
 
 const MapWrapper = ({ clinics }) => {
   const [selected, setSelected] = useState({})
 
-  const onSelect = (item) => {
-    console.log(item)
-  }
+//   const onSelect = (item) => {
+//     console.log(item)
+//   }
 
-  const centerAS = {
-    lat: 44.409286,
-    lng: -68.247501,
-}
+//   const centerAS = {
+//     lat: 44.409286,
+//     lng: -68.247501,
+// }
 
   return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-        <GoogleMap mapContainerClassName='map-class' zoom={11} center={centerAS}>
+    // <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+    //     <GoogleMap mapContainerClassName='map-class' zoom={11} center={centerAS}>
 
-        </GoogleMap>
-    </LoadScript>
+    //     </GoogleMap>
+    // </LoadScript>
+
+    <div style={{
+      height: 300,
+      wigth: 400,
+      background: "beige"
+    }}>
+      Map
+    </div>
   )
 }
 
