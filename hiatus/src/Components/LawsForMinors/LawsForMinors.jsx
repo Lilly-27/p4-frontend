1cb86d4cd3eeac30919d22e30e5f578be93b5026
tellydelly <martinwilson450@gmail.com>
2022-07-15 18:37:55 -0700

    updated laws pertaining to minors component

--- hiatus/src/Components/LawsForMinors/LawsForMinors.jsx
+++ hiatus/src/Components/LawsForMinors/LawsForMinors.jsx
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 const LawsForMinors = ({laws}) => {
+
+  const [policies, setPolicies] = useState() 
+
+const policyFilter = () => {
+  const policy = {}
+
+  if(laws.below_age){
+    policy.below_age=`Restrictions apply for those under the age of ${laws.below_age}`
+  } else {
+    policy.below_age="No restrictions on minors' abortions currently being enforced"
+  }
+  
+  if(laws.parental_consent_required){
+    policy.parentalConsent="Parent must give permission for the minor to have an abortion"
+  } else {
+    policy.parentalConsent="No parental consent required"
+  }
+
+  if(laws.parental_notification_required){
+    policy.parentalNotification="Parental notification is required before of the minor's decision to have an abortion."
+  } else {
+    policy.parentalNotification="No parental notification is required"
+  }
+
+  if(laws.parents_required){
+    if(laws.parents_required === 1){
+      policy.parentRequired = "One parent or guardian must be notified"
+    } else {
+      policy.parentRequired = "Both parents or guardian must be notified"
+    }
+  } else {
+    policy.parentRequired = "No restrictions on minor's abortions"
+  }
+
+  if(laws.judicial_bypass_available){
+    policy.judicialBypass = 'A judge can excuse a minor from the required parental consent and/or notification'
+  } else {
+    policy.judicialBypass = 'No judicial bypass available'
+  }
+
+  if(laws.allows_minor_to_consent){
+    policy.minorConsent='Minors can consent to an abortion'
+  } else {
+    policy.minorConsent='Minors cannot consent to an abortion'
+  }
+
+  return policy
+}
+
+useEffect(() => {
+  const filteredPolicies = policyFilter()
+  setPolicies(filteredPolicies)
+}, [])
+
   return (
     <div className='laws-for-minors'>
         <h2>Laws and Polices Pertaining to Minors</h2>
-        {laws.below_age
+        {/* {laws.below_age
         ? <p>Below age field is present {laws.below_age}</p>
         : <p>No age limits</p>
         }
@@ -27,6 +81,19 @@ const LawsForMinors = ({laws}) => {
         {laws.allows_minor_to_consent
         ? <p>Allows minors to consent</p>
         : <p>Does not all minors to consent</p>
+        } */}
+        {policies
+        ?
+        <>
+        <p>{policies.below_age}</p>
+        <p>{policies.parentalConsent}</p>
+        <p>{policies.parentalNotification}</p>
+        <p>{policies.parentRequired}</p>
+        <p>{policies.judicialBypass}</p>
+        <p>{policies.minorConsent}</p>
+        
+        </>
+        : <p>loading</p>
         }
     </div>
   )
