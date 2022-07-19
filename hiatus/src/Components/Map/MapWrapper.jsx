8114c94c049de4df008ee6d067a2b388a62746aa
Lilly-27 <ghcecilia1@gmail.com>
2022-07-18 19:42:15 -0500

    solving merge conflicts

--- hiatus/src/Components/Map/MapWrapper.jsx
+++ hiatus/src/Components/Map/MapWrapper.jsx
@@ -55,4 +55,40 @@ const MapWrapper = ({ stateName, clinicData }) => {
   )
 }
 
-export default MapWrapper
+export default MapWrapper;
+
+
+
+
+
+
+
+// import {
+//     GoogleMap,
+//     LoadScript,
+//     Marker,
+//     InfoWindow,
+// } from '@react-google-maps/api'
+
+// const MapWrapper = ({ clinics }) => {
+//   const [selected, setSelected] = useState({})
+
+//   const onSelect = (item) => {
+//     console.log(item)
+//   }
+
+//   const centerAS = {
+//     lat: 44.409286,
+//     lng: -68.247501,
+// }
+
+//   return (
+//     <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+//         <GoogleMap mapContainerClassName='map-class' zoom={11} center={centerAS}>
+
+//         </GoogleMap>
+//     </LoadScript>
+//   )
+// }
+
+// export default MapWrapper
