579f8373dad7a161bca79d41ab3f8362b8df67d2
Lilly-27 <ghcecilia1@gmail.com>
2022-07-20 22:02:50 -0500

    Fixed Stories

--- hiatus/src/Pages/Stories/Stories.jsx
+++ hiatus/src/Pages/Stories/Stories.jsx
@@ -2,17 +2,27 @@ import React, { useEffect, useState } from 'react'
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import './Stories.css'
+import './Stories.css';
+
 
 function Stories() {
-  const [storyData, setStoryData] = useState()
+  const [storyData, setStoryData] = useState();
+  const [selected, setSelected] = useState(null)
+
+  const handleButton = (e) => {
+    e.preventDefault();
+    // console.log(e.target.dataset.key)
+    const selectedNumber= Number(e.target.dataset.key)
+    setSelected(selectedNumber)
+  }
+  
   const fetchAll = () => {
     const storyURL = `${process.env.REACT_APP_API_ENDPOINT_PROD}stories/getall`
     fetch(storyURL)
     .then((res) => res.json())
-    .then((storyData)=>{
-      // console.log(storyData)
-      return setStoryData(storyData)
+    .then((data)=>{
+      console.log(data)
+      return setStoryData(data)
     })
     .catch(console.error)
   }
@@ -24,22 +34,29 @@ function Stories() {
   return (
     <div id="Stories">
       <h2>Our Stories Matter</h2>
-      <p>Sharing our stories help us build a sense of community with each other, of acceptance and unity. <br />
+      <p>Sharing our stories helps us build a sense of community with each other, of acceptance and unity. <br />
         Our hope is for you to read these stories and feel comforted by the fact that you are not alone.</p>
-       <Row xs={1} md={3} className="g-4">
-      {Array.from({ length: 9 }).map((_, idx) => (
-        <Col>
-          <Card>
-            <Card.Body>
-              <Card.Title>Title</Card.Title>
-              <Card.Text>
-                Story
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+      <Row xs={1} md={3} xl={8} id="container">
+      {storyData
+      ? storyData.map((story, index) => {
+          return(
+            <Col id="columns">
+              <Card id="card" key={index}>
+                <Card.Body>
+                  <Card.Title>{story.title}</Card.Title>
+                  {selected === index
+                    ? <Card.Text> {story.body}</Card.Text>
+                    :<Card.Text>{story.body.slice(0,500)}</Card.Text>
+                  }
+                </Card.Body>
+                  <button onClick={handleButton} data-key={index}>Read More</button>
+              </Card>
+            </Col>
+          )
+      })
+      :<p>Loading...</p>
+      }
+      </Row>
    
      </div>
   )
