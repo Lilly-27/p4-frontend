a5a8634c20307d803930fca21209260b6de5a733
Lilly-27 <ghcecilia1@gmail.com>
2022-07-21 21:43:11 -0500

    FINISHED FOOTER

--- hiatus/src/Pages/Footer/Footer.jsx
+++ hiatus/src/Pages/Footer/Footer.jsx
@@ -1,49 +1,65 @@
 import React from 'react';
-import Donate from '../Donate/Donate';
 import "./Footer.css"
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Donate from '../Donate/Donate';
+import { Link } from 'react-router-dom';
 
 
 function Footer() {
   return (
     <div id="Footer">
       <div className="footer-container">
-        <div className="team-container">
-          {/* <h6>Meet Hiatus' Devs</h6> */}
-            <div id="dev">
-              <a href="https://www.linkedin.com/in/cecilia-h/"><img src="https://i.imgur.com/AbS6269.jpg" id="img-dev" alt="Cecilia" /></a>
-                <div id="icons">
-                  <a href="https://www.linkedin.com/in/cecilia-h/"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" id="linkedin-icon" alt="LinkedIn icon" /></a>
-                  <a href="https://github.com/Lilly-27"><img src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/github-square-512.png" id="github" alt="github icon" /></a>
-                </div>
-            </div>
+        <Row className="team-container">
+            <Col id="brief">
+              <img 
+                src="https://i.imgur.com/ee28Yvc.png"
+                width="100" 
+                height="100"
+                alt="Hiatus" />
+                <p>Working to help people gain access <br />to information on abortion services post-Roe, <br />regardless of where they live.</p>
+            </Col>
 
-            <div id="dev">
-            <a href="https://www.linkedin.com/in/martin-wilson-tel/"><img src="https://i.imgur.com/Xz3Kk9V.png" id="img-dev" alt="Martin" /></a>
-                <div id="icons">
-                  <a href="https://www.linkedin.com/in/martin-wilson-tel/"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" id="linkedin-icon" alt="LinkedIn icon" /></a>
-                  <a href="https://github.com/TelleDelly"><img src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/github-square-512.png" id="github" alt="github icon" /></a>
-                </div>
-            </div>
-        </div>
+            <Col>
+              <Donate />
+            </Col>
+
+            <Col id="dev">
+                  <h4>Hiatus' Creators</h4>
+               
+                    <h6>Cecilia Hernandez</h6>
+                    <li>
+                      <a href="https://www.linkedin.com/in/cecilia-h/">LinkedIn</a>
+                    </li>
+                    <li>
+                   <a href="https://github.com/Lilly-27">Github</a>
+                   </li>
+                   <br />
+          
+
+                  <h6>Martin Wilson</h6>
+           
+                  <li>
+                    <a href="https://www.linkedin.com/in/martin-wilson-tel/">LinkedIn</a>
+                  </li>
+                  <li>
+                    <a href="https://github.com/TelleDelly">Github</a>
+                  </li>
+          
+                  
+            </Col>
+        </Row>
 
 
 
 
-
-
-
-        <div className="donate">
-          <Donate />
-        </div>
 
         <div className="copyright">
 
         </div>
 
       </div>
-      </div>
+    </div>
   )
 }
 
