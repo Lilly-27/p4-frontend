6e78e686f20655bc8e1dde1bdacaad3f1d35d9a9
Lilly-27 <ghcecilia1@gmail.com>
2022-07-21 19:30:27 -0500

    updated map, footer.

--- hiatus/src/Pages/Footer/Footer.jsx
+++ hiatus/src/Pages/Footer/Footer.jsx
@@ -1,22 +1,48 @@
 import React from 'react';
 import Donate from '../Donate/Donate';
-import Card from 'react-bootstrap/Card';
+import "./Footer.css"
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 
 function Footer() {
   return (
     <div id="Footer">
-      <Card border="light">
-      <h6>Connect with the creators of Hiatus</h6>
-      <ul>
-        Cecilia Hernandez<br />
-        Links to LinkedIn and Github
+      <div className="footer-container">
+        <div className="team-container">
+          {/* <h6>Meet Hiatus' Devs</h6> */}
+            <div id="dev">
+              <a href="https://www.linkedin.com/in/cecilia-h/"><img src="https://i.imgur.com/AbS6269.jpg" id="img-dev" alt="Cecilia" /></a>
+                <div id="icons">
+                  <a href="https://www.linkedin.com/in/cecilia-h/"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" id="linkedin-icon" alt="LinkedIn icon" /></a>
+                  <a href="https://github.com/Lilly-27"><img src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/github-square-512.png" id="github" alt="github icon" /></a>
+                </div>
+            </div>
 
-        Martin Wilson <br />
-        Links to LinkedIn and Github
-      </ul>
+            <div id="dev">
+            <a href="https://www.linkedin.com/in/martin-wilson-tel/"><img src="https://i.imgur.com/Xz3Kk9V.png" id="img-dev" alt="Martin" /></a>
+                <div id="icons">
+                  <a href="https://www.linkedin.com/in/martin-wilson-tel/"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" id="linkedin-icon" alt="LinkedIn icon" /></a>
+                  <a href="https://github.com/TelleDelly"><img src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/github-square-512.png" id="github" alt="github icon" /></a>
+                </div>
+            </div>
+        </div>
 
-      <Donate />
-      </Card>
+
+
+
+
+
+
+        <div className="donate">
+          <Donate />
+        </div>
+
+        <div className="copyright">
+
+        </div>
+
+      </div>
       </div>
   )
 }
