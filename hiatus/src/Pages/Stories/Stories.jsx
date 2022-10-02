b51c4c36f6b191e996fc79fac7c8b28e7c4fc264
TelleDelly <martinwilson450@gmail.com>
2022-10-01 22:23:32 -0700

    Updating story card to remove <h4> as child of <p> warning

--- hiatus/src/Pages/Stories/Stories.jsx
+++ hiatus/src/Pages/Stories/Stories.jsx
@@ -35,11 +35,15 @@ function Stories() {
       <Card id="banner">
       <Card.Img src="https://i.imgur.com/DJnLxtx.png" alt="women supporting women" />
       <Card.ImgOverlay>
-        <Card.Text className="caption">
-          <h3>Our Stories Matter</h3><br />
-          <h4>Our hope is for you to read these stories and <br />
-        feel comforted by the fact that you are not alone.</h4>
+        <Card.Body className='caption'>
+        <Card.Header as='h3'>
+          Our Stories Matter
+        </Card.Header>
+        <Card.Text as='h4'>
+        Our hope is for you to read these stories and <br />
+        feel comforted by the fact that you are not alone.
         </Card.Text>
+        </Card.Body>
       </Card.ImgOverlay>
     </Card>
    
