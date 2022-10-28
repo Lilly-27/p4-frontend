7f1737a1b0b7d0aa2376306ab5ad5ac500843a15
TelleDelly <martinwilson450@gmail.com>
2022-10-27 17:35:32 -0700

    switched svg map to simple-react-maps and created fill color for each state

--- hiatus/src/Components/InteractiveMap/SvgMap.jsx
+++ hiatus/src/Components/InteractiveMap/SvgMap.jsx
@@ -1,59 +1,48 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import { ComposableMap, Geographies, Geography, Graticule } from 'react-simple-maps';
+import React, { useEffect, useState } from 'react';
+import { ComposableMap, Geographies, Geography} from 'react-simple-maps';
+import axios from 'axios'; 
 import getSVGs from '../../Hooks/GetSVGData';
+
+
+
+const svgString = process.env.NODE_ENV === 'production'
+? process.env.REACT_APP_PROG_SVG
+: 'http://localhost:4000/svg/getSVGs'
 
 const geoURL = 'https://raw.githubusercontent.com/deldersveld/topojson/master/countries/united-states/us-albers.json'
 const width = 1100
 
-const SvgMap = () => {
+const SvgMap = ({svgData}) => {
 
-    const svgData = useMemo(() => getSVGs()) 
-    // const svgData = getSVGs()
-    console.log(svgData)
-
-    const findFill = (geo) => {
-      if(svgData !== null){
-        const matchingState = svgData.find((element, index) => {
-          return element.state === geo.properties.name
-        })
-
-        const fill = matchingState["fill"]
-        // console.log(fill)
-      
-        return fill
+  const findFill = (stateName) => {
+    for(let i = 0; i < svgData.length; i++){
+      if(stateName === svgData[i].state){
+        console.log(svgData[i].fill)
+        return svgData[i].fill
+      }
     }
+
   }
 
   return (
-    <ComposableMap
+    svgData !== null
+    ? <ComposableMap
     projection="geoAlbers"
     width={width}
     height={500}
-    // projectionConfig={{
-    //   center: [100, 100]
-    // }}
     >
-      <Geographies geography={geoURL}
-      parseGeographies={(geos) =>{
-        return geos.map((g) => {
-          const fill = findFill(g)
-          console.log(fill)
-          g.properties.fill = fill
-          return g
-        })
-      }}>
+      <Geographies geography={geoURL}>
         {({geographies}) =>
-          geographies.map((geo) => (
-            <>
-            <Geography key={geo.rsmKey} geography={geo} stroke='#ffffff'/>
-            
-            {/* {geographyRender(geo)} */}
-            {/* {console.log(geo)}  */}
-            </>   
-          ))
+          geographies.map((geo, i) => {
+            const fill = findFill(geo.properties.name)
+
+            return <Geography key={geo.rsmKey} geography={geo} fill={fill} stroke='#ffffff'/> 
+        })
+      
         }
       </Geographies>
     </ComposableMap>
+    :<h1>Loading</h1>
   )
 }
 
