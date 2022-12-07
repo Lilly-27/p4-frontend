2af8906828891ed5a2c4857258e61319c0e0e477
TelleDelly <martinwilson450@gmail.com>
2022-12-06 17:31:06 -0800

    loose scroll functionality

--- hiatus/src/Pages/LandingPage/LandingPage.jsx
+++ hiatus/src/Pages/LandingPage/LandingPage.jsx
@@ -5,8 +5,21 @@ import Resources from '../Resources/Resources'
 import Donate from '../Donate/Donate'
 import InteractiveMap from '../../Components/InteractiveMap/InteractiveMap'
 
+import { useParams } from 'react-router'
+import { useEffect } from 'react'
+import { scroller } from 'react-scroll'
+
 
 const LandingPage = () => {
+  const loc = window.location.href
+
+  useEffect(() => {
+    const index = loc.match(/(#)([A-Z | a-z]+)/)
+    setTimeout(() => {
+      document.querySelector(index[0]).scrollIntoView({behavior: 'smooth', block: 'start'})
+    },1000)
+  }, [loc])
+
   return (
     <div id="landing-page">
       <About />
--- hiatus/src/Pages/NavBar/NavBar.jsx
+++ hiatus/src/Pages/NavBar/NavBar.jsx
@@ -4,6 +4,7 @@ import { Navbar, Container, Nav} from 'react-bootstrap';
 import './NavBar.css';
 
 function NavBar() {
+
   return (
     <div className="navbar">
       <Navbar expand="lg">
@@ -17,11 +18,20 @@ function NavBar() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="#About">About</Nav.Link>
-          <Nav.Link href="#map">Map</Nav.Link>
-          <Nav.Link href="#Resources">Resource Hub</Nav.Link>
-          <Nav.Link href="#Stories">Stories</Nav.Link>
-          <Nav.Link href="#Donate">Donate</Nav.Link>
+          <Nav.Link href="/#About">About</Nav.Link>
+          <Nav.Link href="/#map">Map</Nav.Link>
+          <Nav.Link href="/#Resources">Resource Hub</Nav.Link>
+          <Nav.Link href="/#Stories">Stories</Nav.Link>
+          <Nav.Link href="/#Donate">Donate</Nav.Link>
+          {/* <Link path='/#About' >About</Link>
+          <Link path='/#map' >Map</Link>
+          <Link path='/#Resources' >Resources</Link>
+          <Link exact path='/#Stories' >Stories</Link> */}
+          {/* <Link to={{
+            pathname: '/',
+            hash: '#Donate'
+          }}>Donate</Link> */}
+          <Link to='/#Donate'>Donate</Link>
         </Nav>
       </Navbar.Collapse>
       </Container>
